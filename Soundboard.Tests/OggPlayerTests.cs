ec3d96e7e5563a0ad8da274229b7dfebbdd40085
Robert Norell <robert.norell@outlook.com>
2017-10-17 23:09:56 -0500

    Need to be able to do this

--- Soundboard.Tests/OggPlayerTests.cs
+++ Soundboard.Tests/OggPlayerTests.cs
@@ -12,5 +12,19 @@ namespace Soundboard.Tests
         public void Setup()
         {
         }
+
+        [Test]
+        [TestCase(@"example-sounds\example.ogg")]
+        public void It_can_load_an_ogg_file(string pathToOggFile)
+        {
+            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
+
+            Subject.LoadOggFile(theOggFile);
+        }
+
+        private static string GetTheProjectRelativePathForThisProjectFile(string projectFileName)
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, projectFileName);
+        }
     }
 }
