7931c1071e61156edc619bd133b082570e25e79c
Robert Norell <robert.norell@outlook.com>
2017-10-17 23:03:07 -0500

    Move this test to the right class

---
+++ Soundboard.Tests/NVorbisTests/VorbisReaderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Soundboard.Tests.NVorbisTests
+{
+    [TestFixture]
+    public class VorbisReaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(@"example-sounds\example.ogg")]
+        public void It_can_read_an_ogg_file(string pathToOggFile)
+        {
+            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
+
+            using (var vorbis = new NVorbis.VorbisReader(theOggFile))
+            {
+            }
+        }
+
+        private static string GetTheProjectRelativePathForThisProjectFile(string projectFileName)
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, projectFileName);
+        }
+    }
+}
--- Soundboard.Tests/OggPlayerTests.cs
+++ Soundboard.Tests/OggPlayerTests.cs
@@ -4,28 +4,8 @@ using NUnit.Framework;
 
 namespace Soundboard.Tests
 {
-    [TestFixture]
     public class OggPlayerTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
-        [Test]
-        [TestCase(@"example-sounds\example.ogg")]
-        public void It_can_read_an_ogg_file(string pathToOggFile)
-        {
-            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
-
-            using (var vorbis = new NVorbis.VorbisReader(theOggFile))
-            {
-            }
-        }
-
-        private static string GetTheProjectRelativePathForThisProjectFile(string projectFileName)
-        {
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, projectFileName);
-        }
     }
 }
