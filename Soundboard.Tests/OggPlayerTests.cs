efc49ec685adf0b5fc629267d0b6400e648434dd
Robert Norell <robert.norell@outlook.com>
2017-10-17 22:47:00 -0500

    Trust NVorbis to read the file

--- Soundboard.Tests/OggPlayerTests.cs
+++ Soundboard.Tests/OggPlayerTests.cs
@@ -9,5 +9,14 @@ namespace Soundboard.Tests
         public void Setup()
         {
         }
+
+        [Test]
+        [TestCase("example.ogg")]
+        public void It_can_read_an_ogg_file(string pathToOggFile)
+        {
+            using (var vorbis = new NVorbis.VorbisReader(pathToOggFile))
+            {
+            }
+        }
     }
 }
