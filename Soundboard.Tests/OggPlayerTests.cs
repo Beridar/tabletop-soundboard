6ab5b96e542ec21f9d062c5373b668dc97f24675
Robert Norell <robert.norell@outlook.com>
2017-10-17 22:44:10 -0500

    Stub in test class for dealing with ogg files

---
+++ Soundboard.Tests/OggPlayerTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Soundboard.Tests
+{
+    [TestFixture]
+    public class OggPlayerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
