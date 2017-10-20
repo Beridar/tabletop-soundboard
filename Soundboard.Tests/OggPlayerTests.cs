8f36c014c653ff950a05763bd0f50be658364465
Robert Norell <robert.norell@outlook.com>
2017-10-19 21:33:10 -0500

    Assert this

--- Soundboard.Tests/OggPlayerTests.cs
+++ Soundboard.Tests/OggPlayerTests.cs
@@ -38,6 +38,8 @@ namespace Soundboard.Tests
 
             Subject.LoadOggFile(theOggFile);
             Subject.Play();
+
+            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Playing);
         }
 
         [Test]
