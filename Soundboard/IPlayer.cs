0bd43c748f67bcf09b4a6807fd30f2a7c5f8a648
Robert Norell <robert.norell@outlook.com>
2017-10-23 21:22:29 -0500

    This is the same between both ogg and mp3 players

--- Soundboard/IPlayer.cs
+++ Soundboard/IPlayer.cs
@@ -12,4 +12,31 @@ namespace Soundboard
         void Stop();
         PlaybackState CurrentPlaybackState { get; }
     }
+
+    public abstract class Player : IPlayer
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void LoadFile(string filename);
+
+        public Task Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayToCompletion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlaybackState CurrentPlaybackState { get; }
+    }
 }
