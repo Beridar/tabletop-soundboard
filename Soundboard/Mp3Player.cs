89be10927ef5b627cac7fc4e3e2ab99d3da96cd3
Robert Norell <robert.norell@outlook.com>
2017-10-23 21:11:47 -0500

    Framework for MP3 player

---
+++ Soundboard/Mp3Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Soundboard
+{
+    public interface IMp3Player : IPlayer
+    {
+    }
+
+    public class Mp3Player : IMp3Player
+    {
+        public Task Play()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PlayToCompletion()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PlaybackState CurrentPlaybackState { get; }
+    }
+}
