340cf23248c8f3f196f0c89eb5ae36b4394176ca
Robert Norell <robert.norell@outlook.com>
2017-10-23 21:29:36 -0500

    Use this new base class

--- Soundboard/Mp3Player.cs
+++ Soundboard/Mp3Player.cs
@@ -9,44 +9,14 @@ namespace Soundboard
     {
     }
 
-    public class Mp3Player : IMp3Player
+    public class Mp3Player : Player, IMp3Player
     {
-        private WaveOutEvent player;
-        private Mp3FileReader reader;
-
-        public void LoadFile(string filename)
+        public override void LoadFile(string filename)
         {
             Dispose();
 
             reader = new Mp3FileReader(filename);
             player = new WaveOutEvent();
-        }
-
-        public async Task Play()
-        {
-            player.Init(reader);
-            player.Play();
-        }
-
-        public void PlayToCompletion()
-        {
-            Play();
-
-            while (player.PlaybackState == PlaybackState.Playing)
-                Thread.Sleep(50);
-        }
-
-        public void Stop()
-        {
-            player.Stop();
-        }
-
-        public PlaybackState CurrentPlaybackState => player?.PlaybackState ?? PlaybackState.Paused;
-
-        public void Dispose()
-        {
-            player?.Dispose();
-            reader?.Dispose();
         }
     }
 }
