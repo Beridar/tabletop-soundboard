ac5c27fb05799ecd1484712ec1a68c256ff71b61
Robert Norell <robert.norell@outlook.com>
2017-10-24 22:19:50 -0500

    Tests for a soundscape

---
+++ Soundboard.Tests/SoundscapeTests.cs
@@ -0,0 +1,14 @@
+﻿using AutoMoq.Helpers;
+using NUnit.Framework;
+
+namespace Soundboard.Tests
+{
+    [TestFixture]
+    public class SoundscapeTests : AutoMoqTestFixture<Soundscape>
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
