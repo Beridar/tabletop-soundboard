a04907dd3a15c1330245b0621ba1399eec0b1c91
Robert Norell <robert.norell@outlook.com>
2017-10-24 22:45:33 -0500

    Need a way to get any random element from a colleciton

---
+++ Soundboard/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soundboard
+{
+    static class GenericCollectionExtensions
+    {
+        static T GetAnyRandomElement<T>(this IEnumerable<T> collection) where T : new()
+        {
+            return collection.Any()
+                ? collection.ElementAt(new Random().Next(collection.Count()))
+                : default(T);
+        }
+    }
+}
