ff826f08e3f527a2387ed796e56e3aba1a010bf4
Robert Norell <robert.norell@outlook.com>
2017-10-24 22:25:32 -0500

    A soundscape should have a way to manage sounds on the background layer

--- Soundboard.Tests/SoundscapeTests.cs
+++ Soundboard.Tests/SoundscapeTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoMoq.Helpers;
+﻿using System;
+using System.IO;
+using AutoMoq.Helpers;
 using NUnit.Framework;
 
 namespace Soundboard.Tests
@@ -6,9 +8,26 @@ namespace Soundboard.Tests
     [TestFixture]
     public class SoundscapeTests : AutoMoqTestFixture<Soundscape>
     {
+        private IPlayer theBackgroundSound;
+
         [SetUp]
         public void Setup()
         {
+            ResetSubject();
+
+            theBackgroundSound = new OggPlayer();
+            theBackgroundSound.LoadFile(GetTheProjectRelativePathForThisProjectFile(@"example-sounds\mpthreetest.mp3"));
+        }
+
+        private static string GetTheProjectRelativePathForThisProjectFile(string projectFileName)
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, projectFileName);
+        }
+
+        [Test]
+        public void It_should_loop_a_background_sound()
+        {
+            Subject.AddBackgroundSound(theBackgroundSound);
         }
     }
 }
