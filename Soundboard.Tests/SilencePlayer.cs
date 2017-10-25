087f405312fd42eb77c3c3cf16ed72e1819ce921
Robert Norell <robert.norell@outlook.com>
2017-10-24 23:07:38 -0500

    Need this to mock players without actual playback

---
+++ Soundboard.Tests/SilencePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Soundboard.Tests
+{
+    internal class SilencePlayer : IPlayer
+    {
+        public void Dispose()
+        {
+        }
+
+        public void LoadFile(string filename)
+        {
+        }
+
+        public Task Play()
+        {
+        }
+
+        public void PlayToCompletion()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public PlaybackState CurrentPlaybackState { get; private set; }
+    }
+}
