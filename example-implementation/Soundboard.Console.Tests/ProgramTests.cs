178980987e9bfff0df40242028f0f9b3a5dd2690
Robert Norell <robert.norell@outlook.com>
2017-10-25 22:56:56 -0500

    This is as much as we can test Main()

--- example-implementation/Soundboard.Console.Tests/ProgramTests.cs
+++ example-implementation/Soundboard.Console.Tests/ProgramTests.cs
@@ -5,5 +5,10 @@ namespace Soundboard.Console.Tests
     [TestFixture]
     public class ProgramTests
     {
+        [Test]
+        public void It_should_not_crash()
+        {
+            Subject.Main(new string[0]);
+        }
     }
 }
