c442e702183bb37e82d37caab374e2d9a4376757
Robert Norell <robert.norell@outlook.com>
2017-10-30 22:51:07 -0500

    Here's one horrible test

--- Soundboard.Tests/SoundTests.cs
+++ Soundboard.Tests/SoundTests.cs
@@ -1,11 +1,17 @@
 ﻿using AutoMoq.Helpers;
+using NAudio.Wave;
 using NUnit.Framework;
+using Should;
 
 namespace Soundboard.Tests
 {
     [TestFixture]
     public class SoundTests : AutoMoqTestFixture<Sound>
     {
-
+        [Test]
+        public void It_should_be_a_wave_provider()
+        {
+            Subject.ShouldImplement(typeof(IWaveProvider));
+        }
     }
 }
