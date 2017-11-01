14f46604cdabd0f5e909668c207c1f83072951b3
Robert Norell <robert.norell@outlook.com>
2017-10-31 20:31:25 -0500

    Move this util function over

--- Soundboard.Tests/PlayerTests.cs
+++ Soundboard.Tests/PlayerTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoMoq.Helpers;
+﻿using System;
+using System.IO;
+using AutoMoq.Helpers;
 using NAudio.Wave;
 using NUnit.Framework;
 using Should;
@@ -8,6 +10,11 @@ namespace Soundboard.Tests
     [TestFixture]
     public class PlayerTests : AutoMoqTestFixture<Player>
     {
+        private static string GetTheProjectRelativePathForThisProjectFile(string projectFileName)
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, projectFileName);
+        }
+
         [Test]
         [TestCase(@"example-sounds\mpthreetest.mp3")]
         public void It_can_play_an_mp3_file(string pathToSoundFile)
