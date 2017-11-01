93d4b804ad5339a93601ff59a6579e5228e76a26
Robert Norell <robert.norell@outlook.com>
2017-10-31 20:31:02 -0500

    Pull this test over

--- Soundboard.Tests/Mp3SoundTests.cs
+++ Soundboard.Tests/Mp3SoundTests.cs
@@ -32,18 +32,6 @@ namespace Soundboard.Tests
 
         [Test]
         [TestCase(@"example-sounds\mpthreetest.mp3")]
-        public void It_can_play_an_mp3_file(string pathToSoundFile)
-        {
-            var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
-
-            Subject.LoadFile(theSoundFile);
-            Subject.Play();
-
-            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Playing);
-        }
-
-        [Test]
-        [TestCase(@"example-sounds\mpthreetest.mp3")]
         public void It_can_block_until_playback_is_complete(string pathToSoundFile)
         {
             var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
--- Soundboard.Tests/PlayerTests.cs
+++ Soundboard.Tests/PlayerTests.cs
@@ -1,6 +1,23 @@
-﻿namespace Soundboard.Tests
+﻿using AutoMoq.Helpers;
+using NAudio.Wave;
+using NUnit.Framework;
+using Should;
+
+namespace Soundboard.Tests
 {
-    public class PlayerTests
+    [TestFixture]
+    public class PlayerTests : AutoMoqTestFixture<Player>
     {
+        [Test]
+        [TestCase(@"example-sounds\mpthreetest.mp3")]
+        public void It_can_play_an_mp3_file(string pathToSoundFile)
+        {
+            var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
+
+            Subject.LoadFile(theSoundFile);
+            Subject.Play();
+
+            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Playing);
+        }
     }
 }
