1b5ee8f9075facb34bfd26b12a23ba4e7fc1d8c5
Robert Norell <robert.norell@outlook.com>
2017-10-31 20:09:14 -0500

    Force this on children as well

--- Soundboard/Sound.cs
+++ Soundboard/Sound.cs
@@ -5,7 +5,6 @@ namespace Soundboard
     public abstract class Sound : IWaveProvider
     {
         public abstract int Read(byte[] buffer, int offset, int count);
-
-        public WaveFormat WaveFormat { get; }
+        public abstract WaveFormat WaveFormat { get; }
     }
 }
