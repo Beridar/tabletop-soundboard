390649b35fd24db1dc87466904a73d0b7367c932
Robert Norell <robert.norell@outlook.com>
2017-10-31 22:38:59 -0500

    Audio sources are sounds, not players

---
+++ Soundboard/OggSound.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+
+namespace Soundboard
+{
+    public interface IOggSound : ISound
+    {
+    }
+
+    public class OggSound : Sound, IOggSound
+    {
+        public void LoadFile(string filename)
+        {
+            Dispose();
+
+            reader = new VorbisWaveReader(filename);
+            player = new WaveOutEvent();
+        }
+    }
+}
