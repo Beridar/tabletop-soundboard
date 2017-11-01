d9ebbe0737fa5bcfc8e8d420d17b06af183c42eb
Robert Norell <robert.norell@outlook.com>
2017-10-31 22:50:04 -0500

    These tests should be combined

--- Soundboard.Tests/PlayerTests.cs
+++ Soundboard.Tests/PlayerTests.cs
@@ -18,7 +18,8 @@ namespace Soundboard.Tests
 
         [Test]
         [TestCase(@"example-sounds\mpthreetest.mp3")]
-        public void It_can_play_an_mp3_file(string pathToSoundFile)
+        [TestCase(@"example-sounds\example.ogg")]
+        public void It_can_play_a_sound(string pathToSoundFile)
         {
             var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
             var theMp3File = new Mp3Sound(theSoundFile);
@@ -31,6 +32,7 @@ namespace Soundboard.Tests
 
         [Test]
         [TestCase(@"example-sounds\mpthreetest.mp3")]
+        [TestCase(@"example-sounds\example.ogg")]
         public void It_can_block_until_mp3_playback_is_complete(string pathToSoundFile)
         {
             var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
@@ -44,63 +46,13 @@ namespace Soundboard.Tests
 
         [Test]
         [TestCase(@"example-sounds\mpthreetest.mp3")]
+        [TestCase(@"example-sounds\example.ogg")]
         public void It_can_stop_mp3_playback_before_playback_is_complete(string pathToSoundFile)
         {
             var theSoundFile = GetTheProjectRelativePathForThisProjectFile(pathToSoundFile);
             var theMp3File = new Mp3Sound(theSoundFile);
 
             Subject.Load(theMp3File);
-            Subject.Play();
-            Thread.Sleep(500);
-            Subject.Stop();
-
-            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Stopped);
-        }
-
-        [Test]
-        [TestCase(@"example-sounds\example.ogg")]
-        public void It_can_load_an_ogg_file(string pathToOggFile)
-        {
-            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
-            var theOggSound = new OggSound(theOggFile);
-
-            Subject.Load(theOggSound);
-        }
-
-        [Test]
-        [TestCase(@"example-sounds\example.ogg")]
-        public void It_can_play_an_ogg_file(string pathToOggFile)
-        {
-            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
-            var theOggSound = new OggSound(theOggFile);
-
-            Subject.Load(theOggSound);
-            Subject.Play();
-
-            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Playing);
-        }
-
-        [Test]
-        [TestCase(@"example-sounds\example.ogg")]
-        public void It_can_block_until_ogg_playback_is_complete(string pathToOggFile)
-        {
-            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
-            var theOggSound = new OggSound(theOggFile);
-
-            Subject.Load(theOggSound);
-            Subject.PlayToCompletion();
-
-            Subject.CurrentPlaybackState.ShouldEqual(PlaybackState.Stopped);
-        }
-
-        [Test]
-        [TestCase(@"example-sounds\example.ogg")]
-        public void It_can_stop_ogg_playback_before_playback_is_complete(string pathToOggFile)
-        {
-            var theOggFile = GetTheProjectRelativePathForThisProjectFile(pathToOggFile);
-            var theOggSound = new OggSound(theOggFile);
-
-            Subject.Load(theOggSound);
             Subject.Play();
             Thread.Sleep(500);
             Subject.Stop();
