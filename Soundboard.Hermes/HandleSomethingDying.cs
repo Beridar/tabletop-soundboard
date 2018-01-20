fe2d1cc7c0b16cd4a6bcbaa7378108326e18489b
Robert Norell <robert.norell@outlook.com>
2018-01-20 11:46:01 -0600

    Now we can play a willhelm

--- Soundboard.Hermes/HandleSomethingDying.cs
+++ Soundboard.Hermes/HandleSomethingDying.cs
@@ -1,21 +1,46 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Soundboard;
 
 namespace Soundboard.Hermes
 {
     class HandleSomethingDying : IEventHandler
     {
+        private static Dictionary<string, Sound> SoundsByType;
+        private static WaveOutEvent WavePlayer;
+
+        static HandleSomethingDying()
+        {
+            SoundsByType = new Dictionary<string, Sound>
+            {
+                ["Willhelm"] = new OggSound("willhelm.ogg"),
+                ["Bones01"] = new OggSound("bones tag 01 basic.ogg")
+            };
+
+            WavePlayer = new WaveOutEvent();
+        }
+
+        private readonly Player player;
+
+        public HandleSomethingDying()
+        {
+            player = new Player(WavePlayer);
+
+            player.Load(SoundsByType.FirstOrDefault().Value);
+        }
+
         public bool CanHandle(string @event)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public void Handle(string @event)
         {
-            throw new NotImplementedException();
+            player.PlayToCompletion();
         }
     }
 }
