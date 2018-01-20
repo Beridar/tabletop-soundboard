5c7b15b24fb80fbcd60009e529845545fb69fe43
Robert Norell <robert.norell@outlook.com>
2018-01-20 11:14:30 -0600

    We will end up having many events, but need to start with something dying.

---
+++ Soundboard.Hermes/HandleSomethingDying.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Hermes
+{
+    class HandleSomethingDying : IEventHandler
+    {
+        public bool CanHandle(string @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(string @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Soundboard.Hermes/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Hermes
+{
+    public interface IEventHandler
+    {
+        bool CanHandle(string @event);
+        void Handle(string @event);
+    }
+}
